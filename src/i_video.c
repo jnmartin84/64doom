8a96a8136218cb614c93eff4e3e605f15a578428
Jason Martin <jnmartin84@gmail.com>
2023-05-16 11:35:39 -0400

    go back to simpler I_SetPalette

--- src/i_video.c
+++ src/i_video.c
@@ -133,55 +133,18 @@ void I_RestorePalette(void)
 void I_SetPalette(byte* palette)
 {
     const byte *gammaptr = gammatable[usegamma];
-	byte p1,p2,p3;
-    byte r,g,b;
+
+    unsigned int r,g,b;
     unsigned int i;
+    
+    for (i = 0; i < 256; i++)
+    {
+        r = gammaptr[*palette++];
+        g = gammaptr[*palette++];
+        b = gammaptr[*palette++];
 
-    // this is ugly from a well-intentioned but probably pointless attempt to make it oPtImIzEd 
-    for (i = 0; i < 768/12; i++) {
-
-		uint32_t fc1 = *(uint32_t *)(&palette[i*12]);
-		uint32_t fc2 = *(uint32_t *)(&palette[(i*12)+4]);
-		uint32_t fc3 = *(uint32_t *)(&palette[(i*12)+8]);
-		
-		p1 = fc1 >> 24;
-		p2 = fc1 >> 16;
-		p3 = fc1 >> 8;
-		
-        r = gammaptr[p1];
-        g = gammaptr[p2];
-        b = gammaptr[p3];
-        current_palarray[i*4] = graphics_make_color(r,g,b,0xff);
-
-		p1 = fc1 & 0xff;
-		p2 = fc2 >> 24;
-		p3 = fc2 >> 16;
-		
-        r = gammaptr[p1];
-        g = gammaptr[p2];
-        b = gammaptr[p3];
-        current_palarray[(i*4)+1] = graphics_make_color(r,g,b,0xff);
-
-		p1 = fc2 >> 8;
-		p2 = fc2 & 0xff;
-		p3 = fc3 >> 24;
-		
-        r = gammaptr[p1];
-        g = gammaptr[p2];
-        b = gammaptr[p3];
-        current_palarray[(i*4)+2] = graphics_make_color(r,g,b,0xff);
-
-		p1 = fc3 >> 16;
-		p2 = fc3 >> 8;
-		p3 = fc3 & 0xff;
-		
-        r = gammaptr[p1];
-        g = gammaptr[p2];
-        b = gammaptr[p3];
-        current_palarray[(i*4)+3] = graphics_make_color(r,g,b,0xff);
-	}
-
-	palette += 256*3;
+        current_palarray[i] = graphics_make_color(r,g,b,0xff);
+    }
 }
 
 #include "z_zone.h"
